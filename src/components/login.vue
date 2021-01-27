eb5b37d7da63ef46b4236a3bb9fdf7bb74dc51b3
lele <562282501@qq.com>
2021-01-27 16:47:17 +0800

    登录组件页面搭建

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="login_container">
-    <div class="login_box"></div>
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'login',
-  data() {
-    return {}
-  },
-  methods: {}
-}
+export default {}
 </script>
 
 <style lang="less" scoped>
@@ -23,9 +38,39 @@ export default {
   width: 450px;
   height: 300px;
   background-color: #fff;
+  border-radius: 3px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//引入elementUi的包
 import './plugins/element.js'
+//引入全局的css文件
 import './assets/css/globle.css'
+//引入字体图标
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 
 new Vue({
