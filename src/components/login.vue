0563f3074803d7ee8009ad70734d20ffcef602bc
lele <562282501@qq.com>
2021-01-27 14:59:56 +0800

    梳理项目结构

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    登录组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import login from '../components/login.vue'
+//注册路由组件
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', component: login },
+  { path: '/login', component: login }
 ]
 
 const router = new VueRouter({
