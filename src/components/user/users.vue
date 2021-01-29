4d7a110aead3768af4c2a547208c781fed403903
lele <562282501@qq.com>
2021-01-29 16:25:29 +0800

    配置用户列表子路由

---
+++ src/components/user/users.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>users组件</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,22 @@ import VueRouter from 'vue-router'
 import login from '../components/login.vue'
 import home from '../components/home.vue'
 import welcome from '../components/welcome.vue'
+import users from '../components/user/users.vue'
 //注册路由组件
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: login },
   { path: '/login', component: login },
-  { path: '/home', component: home, redirect: '/welcome', children: [{ path: '/welcome', component: welcome }] }
+  {
+    path: '/home',
+    component: home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: welcome },
+      { path: '/users', component: users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
