028a622c6cd93c8824d8d411ba6ee3f84078ad4f
lele <562282501@qq.com>
2021-01-29 11:37:38 +0800

    Home

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button> </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: 'Home',
-
   data() {
     return {}
   },
-
   methods: {
     logout() {
       window.sessionStorage.clear()
@@ -21,4 +23,17 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
