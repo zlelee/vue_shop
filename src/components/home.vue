3ccd5c31dd723fbe94ba4e24ab6f84ba23c67a6f
lele <562282501@qq.com>
2021-01-29 11:56:51 +0800

    Header结构及样式调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container class="home-container">
-    <el-header>Header<el-button type="info" @click="logout">退出</el-button> </el-header>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -28,7 +34,20 @@ export default {
   height: 100%;
 }
 .el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #373d41;
+  padding-left: 0;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
