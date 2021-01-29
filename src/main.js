ccb1cb4894faaf5a81139bd43c8b69f61b8c943a
lele <562282501@qq.com>
2021-01-29 14:31:57 +0800

    添加Authorization

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 import axios from 'axios'
 axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
