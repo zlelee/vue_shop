dd038c195b0a32dd7ea4951dbb3139da92ec4d89
lele <562282501@qq.com>
2021-01-29 17:17:17 +0800

    绘制用户列表组件的基本结构

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, MenuItem, Submenu } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, MenuItem, Submenu, Breadcrumb, BreadcrumbItem, Row, Col, Card } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -12,4 +12,9 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(MenuItem)
 Vue.use(Submenu)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
 Vue.prototype.$message = Message
