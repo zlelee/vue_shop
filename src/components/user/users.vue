7a8bd132a51cf4f264cea0616aa162b249dc32fa
lele <562282501@qq.com>
2021-01-30 11:41:43 +0800

    渲染添加对话框的表单

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -41,8 +41,21 @@
       </el-table>
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryObj.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryObj.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
     </el-card>
-    <el-dialog title="提示" :visible.sync="addDialogVisible" width="30%">
-      <span>这是一段信息</span>
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="30%">
+      <el-form :model="addForm" :rules="addFormrules" ref="addFormRef" label-width="70px" class="demo-ruleForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
@@ -64,7 +77,28 @@ export default {
       },
       userList: [],
       total: 0,
-      addDialogVisible: false
+      addDialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      addFormrules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ],
+        email: [{ required: true, message: '请输入邮箱', trigger: 'blur' }],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { min: 11, max: 11, message: '请输入11位数字的手机号', trigger: 'blur' }
+        ]
+      }
     }
   },
 
