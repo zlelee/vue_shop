05ec26457edfcf41800c4783df42f738efcdd678
lele <562282501@qq.com>
2021-01-30 17:11:36 +0800

    获取权限列表数据

--- src/components/power/rights.vue
+++ src/components/power/rights.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
-    rights组件
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">权限管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'rights',
 
   data() {
-    return {}
+    return {
+      rightsList: []
+    }
   },
 
-  methods: {}
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) return this.$message.error('获取权限列表数据失败')
+      this.rightsList = res.data
+    }
+  },
+  created() {
+    this.getRightsList()
+  }
 }
 </script>
 
