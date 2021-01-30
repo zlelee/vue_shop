87d0cf1ff550869e7f1a362d4f2e7119482bf861
lele <562282501@qq.com>
2021-01-30 14:30:01 +0800

    添加用户功能

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -58,7 +58,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -139,6 +139,16 @@ export default {
     },
     addFormClose() {
       this.$refs.addFormRef.resetFields()
+    },
+    addUser() {
+      this.$refs.addFormRef.validate(async (flag) => {
+        if (!flag) return this.$message.error('请输入合法的用户信息')
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) return this.$message.error('添加用户失败')
+        this.$message.success('添加用户成功')
+        this.addDialogVisible = false
+        this.getUserList()
+      })
     }
   },
   created() {
