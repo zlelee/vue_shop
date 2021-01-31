b44e34f17909cca7fb7f45cecdb0eb5fec021dc3
lele <562282501@qq.com>
2021-01-31 16:04:10 +0800

    配置商品管理组件

---
+++ src/components/goods/cate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
