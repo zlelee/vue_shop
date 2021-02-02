f02697241c3e4392027219d1fce4e5fe3dcc1bd3
lele <562282501@qq.com>
2021-02-02 11:26:14 +0800

    渲染Tab栏

--- src/components/goods/params.vue
+++ src/components/goods/params.vue
@@ -16,8 +16,12 @@
         </el-col>
       </el-row>
       <el-tabs v-model="activeName" @tab-click="tabChangeClick">
-        <el-tab-pane label="动态参数" name="first">动态参数</el-tab-pane>
-        <el-tab-pane label="静态属性" name="second">静态属性</el-tab-pane>
+        <el-tab-pane label="动态参数" name="first">
+          <el-button type="primary" :disabled="isDisabled">添加参数</el-button>
+        </el-tab-pane>
+        <el-tab-pane label="静态属性" name="second">
+          <el-button type="primary" :disabled="isDisabled">添加属性</el-button>
+        </el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -56,6 +60,11 @@ export default {
       }
     },
     tabChangeClick() {}
+  },
+  computed: {
+    isDisabled() {
+      return this.selectedCateKeys.length !== 3
+    }
   }
 }
 </script>
