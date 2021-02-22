bfebda1824010b2b90d421894aac7ca8124285f3
lele <562282501@qq.com>
2021-02-22 11:13:27 +0800

    实现省市区县的数据联动效果

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -32,12 +32,13 @@
         <el-table-column label="操作">
           <template>
             <el-button size="mini" type="primary" icon="el-icon-edit" @click="showAddressDialog"></el-button>
-            <el-button size="mini" type="success" icon="el-icon-location"></el-button>
+            <el-button size="mini" type="success" icon="el-icon-location" @click="showProgressDialog"></el-button>
           </template>
         </el-table-column>
       </el-table>
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[3, 5, 10, 20]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
     </el-card>
+    <!-- 修改地址的对话框 -->
     <el-dialog title="修改地址" :visible.sync="addressDialogVisible" width="30%" @close="closeAddressDialog">
       <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px" class="demo-ruleForm">
         <el-form-item label="省市区/县" prop="address1">
@@ -50,6 +51,14 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="addressDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addressDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 展示物流进度的对话框 -->
+    <el-dialog title="物流进度" :visible.sync="progressVisible" width="50%">
+      <span>这是一个dialog</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="progressVisible = false">取 消</el-button>
+        <el-button type="primary" @click="progressVisible = false">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -78,7 +87,8 @@ export default {
         address1: [{ required: true, message: '请选择省市区/县', trigger: 'blur' }],
         address2: [{ required: true, message: '请填写详细地址', trigger: 'blur' }]
       },
-      cityDada
+      cityDada,
+      progressVisible: false
     }
   },
   created() {
@@ -107,6 +117,9 @@ export default {
     closeAddressDialog() {
       this.addressDialogVisible = false
       this.$refs.addressFormRef.resetFields()
+    },
+    async showProgressDialog() {
+      this.progressVisible = true
     }
   }
 }
